81f76d08a69eb5d39e86010cc594046ec0f71b38
Nadeem Shabir <nadeem.shabir@talis.com>
2013-03-22 23:56:19 +0000

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('yeomanExampleApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
